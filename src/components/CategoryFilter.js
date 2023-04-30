eaeb0d6a3babfeea197b3bc10e5113e73f850cdd
Cynthia Nakhumicha Barasa <cynthiabarasa312@gmail.com>
2023-04-30 09:25:57 +0300

    Completed task

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,24 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories, selectedCategory, onSelectCategory }) {
+
+  const categoryButton = categories.map((category) => {
+    const className = category === selectedCategory ? "selected" : "";
+    return (
+      <button
+        key={category}
+        className={className}
+        onClick={(event) => onSelectCategory(category, event)}
+      >
+        {category}
+      </button>
+    );
+  });
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categoryButton}
     </div>
   );
 }
